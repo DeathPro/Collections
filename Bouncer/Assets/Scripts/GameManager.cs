cf22bf17b6f30547ec32543b3c77483953bb3efa
Muhammed Resul BİLKİL <garnnet_221@hotmail.com>
2018-05-07 23:32:23 +0600

    Simple Game
    Simple Game

---
+++ Bouncer/Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class GameManager : MonoBehaviour {
+
+    bool gameEnd = false;
+
+    public float restartDelay = 1;
+
+    public GameObject completeLevelUI;
+
+    public void CompleteLevel()
+    {
+        completeLevelUI.SetActive(true);
+    }
+
+    public void EndGame()
+    { 
+        if (gameEnd == false)
+        {
+            gameEnd = true;
+            Debug.Log("GAME OVER");
+            // Restart Game :D
+            Invoke("Restart", restartDelay);
+        }
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+}
